c1717d84e5c368e6f04253cc2882fa1da9eaf64e
LonelySol <pasted.virus@gmail.com>
2022-10-31 00:37:23 +0900

    Update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 require('dotenv').config({path: '.env'});
-const port = 3000 | process.env.PORT
+const port = process.env.PORT | 3000
 
 
 server.listen(port, () => {
