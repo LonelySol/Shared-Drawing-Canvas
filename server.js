6e90c8a1bc332fc6cbf5af61b83f1ebecd0ae1af
LonelySol <pasted.virus@gmail.com>
2022-10-31 19:37:54 +0900

    Update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT | 3000
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
-
+app.use(express.static('/public'));
 server.listen(port, () => {
     console.log(`Listening on ${port}`);
 });
